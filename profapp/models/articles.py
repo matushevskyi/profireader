b70032d19dbf255c1e05fd89baa90788b2ce1681
Oles Zaburannyi <kakabomba@gmail.com>
2016-03-30 13:26:56 +0300

    m

--- profapp/models/articles.py
+++ profapp/models/articles.py
@@ -48,7 +48,7 @@ class ArticlePortalDivision(Base, PRBase):
     status = Column(TABLE_TYPES['status'], default='SUBMITTED')
     STATUSES = {'SUBMITTED': 'SUBMITTED', 'UNPUBLISHED': 'UNPUBLISHED', 'PUBLISHED': 'PUBLISHED', 'DELETED': 'DELETED'}
 
-    visibility = Column(TABLE_TYPES['status'], default='REGISTERED')
+    visibility = Column(TABLE_TYPES['status'], default='OPEN')
     like_count = Column(TABLE_TYPES['int'], default=0)
     VISIBILITIES = {'OPEN': 'OPEN', 'REGISTERED': 'REGISTERED', 'PAYED': 'PAYED', 'CONFIDENTIAL': 'CONFIDENTIAL'}
 
