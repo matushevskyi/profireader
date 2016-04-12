14e7c2933c2b9945318d348f7b14ca44e1e832c2
steve <stopa6767@gmail.com>
2016-04-12 14:47:04 +0300

    rewrite func "portals_list_load"

--- profapp/controllers/views_index.py
+++ profapp/controllers/views_index.py
@@ -35,8 +35,10 @@ def portals_list():
 @ok
 def portals_list_load(json):
     ret, page, page2 = Search().search(
-        {'class': Portal, 'return_fields': 'default_dict'},
-        page=1, search_text=json.get('text'), pagination=True, items_per_page=5*json.get('next_page'))
+        {'class': Portal,
+         'filter':(~Portal.id.in_(db(UserPortalReader.portal_id).filter(UserPortalReader.user_id==g.user.id).all())) if g.user else None,
+         'return_fields': 'default_dict'},
+          page=1, search_text=json.get('text'), pagination=True, items_per_page=5 * json.get('next_page'))
     return {'list_portals':[PRBase.merge_dicts(p, {'subscribed': True if UserPortalReader.get(portal_id=p_id) else False})
             for p_id, p in ret.items()], 'end': True if page == 1 or page == 0 else False}
 
