96593009cfa706ec53664b0b32ab53a4e7255207
Oles Zaburannyi <kakabomba@gmail.com>
2016-08-13 18:47:04 -0400

    apache ssl

--- utils/get_ssl_for_domain.sh
+++ utils/get_ssl_for_domain.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [[ "$1" == '' ]]; then
+  echo "usage: $0 httpdir_where_leth_encrypt_put_data_for_domain_ownership_validation main.domain [anoter.doamins... ]"
+  exit
+fi
+
 httpdir=$1
 maindomain=$2
 args=($@)
