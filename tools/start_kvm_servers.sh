5c8524e67cd1d61aedebd314b44a2abe69b01a6f
Oles Zaburrannyi <kakabomba@gmail.com>
2017-06-29 21:39:14 +0300

    m

--- tools/start_kvm_servers.sh
+++ tools/start_kvm_servers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo kvm ${@:1} -m 2048 -device e1000,netdev=net10,mac=00:10:01:01:01:00 -netdev tap,id=net10,script=/root/create_tun.sh -drive format=raw,file=/root/elk.profi.raw &
-sudo kvm ${@:1} -m 512 -device e1000,netdev=net10,mac=00:10:01:01:01:10 -netdev tap,id=net10,script=/root/create_tun.sh -drive format=raw,file=/root/postgres.profi.raw &
+sudo kvm ${@:1} -m 1024 -device e1000,netdev=net10,mac=00:10:01:01:01:00 -netdev tap,id=net10,script=/root/create_tun.sh -drive format=raw,file=/root/elk.profi.raw &
+sudo kvm ${@:1} -m 2048  -smp cores=2,threads=1,sockets=1 -device e1000,netdev=net10,mac=00:10:01:01:01:10 -netdev tap,id=net10,script=/root/create_tun.sh -drive format=raw,file=/root/postgres.profi.raw &
 
