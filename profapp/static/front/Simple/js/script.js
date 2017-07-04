ac7d621b54a4dc9bbef1321ce22a1461eb750575
Oles Zaburrannyi <kakabomba@gmail.com>
2017-07-04 13:27:02 +0300

    m

--- profapp/static/front/Simple/js/script.js
+++ profapp/static/front/Simple/js/script.js
@@ -23,35 +23,6 @@ function add_message(message, type) {
 
 }
 
-set_all_images_galleries = function (url, callback, $container) {
-    var set_item = function ($img, item) {
-        $img.css({backgroundImage: 'url(' + fileUrl(item['file_id']) + ')'});
-        $img.attr('title', item['title'] + "\n" + item['copyright']);
-    };
-    var $cont = $($container ? $container : 'body');
-    var callback = callback ? callback : function ($img, gallery_data) {
-            $img.data('image_gallery_selected_index', 0);
-            $img.data('image_gallery_data', gallery_data);
-
-            set_item($img, gallery_data['items'][0]);
-
-            $img.on('click', function (e) {
-                var data = $img.data('image_gallery_data');
-                var index = $img.data('image_gallery_selected_index');
-                index = index + 1;
-                index = (index >= data.items.length ? 0 : index);
-                $img.data('image_gallery_selected_index', index);
-                set_item($img, gallery_data['items'][index]);
-            });
-        };
-
-    $('img.data-mce-image-gallery-placeholder', $cont).each(function (ind, img) {
-        $ok(url, {'gallery_id': $(img).attr('data-mce-image-gallery-placeholder')}, function (resp) {
-            callback($(img), resp);
-        });
-    });
-};
-
 function fileUrl(id, down, if_no_file) {
 
     if (!id) return (if_no_file ? if_no_file : '');
