86b113ccd52db06f3410b84d27f503ef022c3505
Oles Zanurannyi <kakabomba@gmail.com>
2017-09-25 21:45:55 +0300

    m

---
+++ tools/run_with_env_loop.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+basen=$(basename "$1" | cut -d. -f1)
+source ../.venv/bin/activate
+while true
+do
+    python $2 ${@:3} 2>&1 | logger -t $basen
+	sleep 1
+	sleep $1
+done
+
+
+
+
+
